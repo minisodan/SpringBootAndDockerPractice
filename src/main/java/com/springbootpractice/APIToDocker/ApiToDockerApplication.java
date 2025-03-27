4247962c866b96a9a15ba2b85dc35b937afbd646
Mason Myles <59033177+minisodan@users.noreply.github.com>
2025-03-26 22:16:48 -0500

    initial changes

---
+++ src/main/java/com/springbootpractice/APIToDocker/ApiToDockerApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpractice.APIToDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiToDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiToDockerApplication.class, args); // entry point to the application
+	}
+
+}
---
+++ src/test/java/com/springbootpractice/APIToDocker/ApiToDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootpractice.APIToDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiToDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
